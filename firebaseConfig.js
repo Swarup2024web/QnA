bebefd384c468857b2265b268577a3f11c970553
Swarup2024web <bswarup1998@gmail.com>
2025-07-24 11:43:35 +0530

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,10 +1,7 @@
-// Import Firebase libraries
-// Make sure to include this script in your HTML or build process
-// Use Firebase CDN version for simplicity in browser environments
+// FirebaseConfig.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.24.0/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/9.24.0/firebase-firestore.js";
 
-// Your Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyApDnDrhy_Z0zvLoWfvsmP4JJSUIdtSulM",
   authDomain: "qnasite-6d9cc.firebaseapp.com",
@@ -14,11 +11,7 @@ const firebaseConfig = {
   appId: "1:707268852195:web:05e13ee17b0d96253688d6"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-// Get Firestore instance
 const db = getFirestore(app);
 
-// Export the database instance
 export { db };
