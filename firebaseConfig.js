db3ba428172cbdbfd1a83ba9fc1c4daa2b428248
Swarup2024web <bswarup1998@gmail.com>
2025-07-24 13:53:04 +0530

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -6,12 +6,11 @@ const firebaseConfig = {
   apiKey: "AIzaSyApDnDrhy_Z0zvLoWfvsmP4JJSUIdtSulM",
   authDomain: "qnasite-6d9cc.firebaseapp.com",
   projectId: "qnasite-6d9cc",
-  storageBucket: "qnasite-6d9cc.firebasestorage.app",
+  storageBucket: "qnasite-6d9cc.appspot.com",
   messagingSenderId: "707268852195",
   appId: "1:707268852195:web:05e13ee17b0d96253688d6"
 };
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
-
 export { db };
